211f518fdcc1ea92088fe09fd49341110afed843
InJeCTrL <dap1933@hotmail.com>
2018-09-09 01:16:16 +0800

    连上摄像头

---
+++ Market/CheckGoods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.FFMPEG;
+using AForge.Controls;
+
+namespace Market
+{
+    /// <summary> 调用摄像头，识别BarCode，联网查询BarCode对应商品信息
+    /// </summary>
+    class CheckGoods
+    {
+        /// <summary> 输入视频设备列表
+        /// </summary>
+        private FilterInfoCollection VideoDevices;
+        /// <summary> 用于获取输入的视频设备
+        /// </summary>
+        private VideoCaptureDevice VideoSource;
+        /// <summary> 初始化商品扫描
+        /// </summary>
+        public CheckGoods()
+        {
+
+        }
+        /// <summary> 检查视频输入设备是否正常
+        /// </summary>
+        /// <returns>返回 true：正常找到 false：异常</returns>
+        public Boolean CheckVideoDevice()
+        {
+            VideoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//枚举所有视频输入设备
+            if (VideoDevices.Count == 0)//未找到可用的视频输入设备
+                return false;//返回false
+            else
+                return true;//返回true
+        }
+        /// <summary> 设置视频输入源
+        /// </summary>
+        public void SetVideoSource()
+        {
+            VideoSource = new VideoCaptureDevice(VideoDevices[0].MonikerString);//获取视频输入源，默认连接第一个输入设备
+            VideoSource.VideoResolution = VideoSource.VideoCapabilities[0];//指定视频输出配置
+        }
+        /// <summary> 获得视频输入源
+        /// </summary>
+        /// <returns>返回 视频输入源</returns>
+        public VideoCaptureDevice GetVideoSource()
+        {
+            return VideoSource;//返回视频输入源
+        }
+    }
+}
---
+++ Market/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Controls;
+
+namespace Market
+{
+    /// <summary> 收银主窗体
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary> 使收银主窗体全局可访问
+        /// </summary>
+        public static Form1 MainFrm;
+        /// <summary> 商品扫描并检查
+        /// </summary>
+        private CheckGoods ChkGoods = new CheckGoods();
+        /// <summary> 收银主窗体初始化，初始化摄像头
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            if (InitCamera() == false)
+            {
+                button2.Enabled = false;//不允许开始营业
+                MessageBox.Show(null, "摄像头初始化失败，请检查设备！", "设备异常");//若摄像头初始化失败则报错
+            }
+        }
+        /// <summary> 询问是否退出超市供销系统，并执行相应操作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show(null, "是否要退出系统？", "退出确认", MessageBoxButtons.YesNo))
+            {
+                StopCamera();//关闭摄像头
+                Application.Exit();//退出程序
+            }
+        }
+        /// <summary> 窗体显示时设置各控件位置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            button1.SetBounds(this.Size.Width - 110 - 30, 30, 111, 44);//设置关闭系统按钮位置
+        }
+        /// <summary> 初始化摄像头
+        /// </summary>
+        /// <returns>返回 true：成功 false：失败</returns>
+        private Boolean InitCamera()
+        {
+            if (ChkGoods.CheckVideoDevice() == false)//检查视频输入设备
+                return false;//不存在视频输入设备返回false
+            else
+            {
+                ChkGoods.SetVideoSource();//设置视频输入源
+                videoSourcePlayer1.VideoSource = ChkGoods.GetVideoSource();//将收银窗体中视频控件与视频输入设备关联
+                return true;
+            }
+        }
+        /// <summary> 开启摄像头，开始捕获
+        /// </summary>
+        private void StartCamera()
+        {
+            videoSourcePlayer1.Start();//开启摄像头
+        }
+        /// <summary> 关闭摄像头，停止捕获
+        /// </summary>
+        private void StopCamera()
+        {
+            videoSourcePlayer1.Stop();
+        }
+        /// <summary> 开始营业按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartCamera();//开启摄像头
+            button3.Enabled = true;//允许暂停营业
+            button2.Enabled = false;//不允许多次开始营业
+        }
+        /// <summary> 暂停营业按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StopCamera();//关闭摄像头
+            button3.Enabled = false;//允许开始营业
+            button2.Enabled = true;//不允许多次暂停营业
+        }
+    }
+}
